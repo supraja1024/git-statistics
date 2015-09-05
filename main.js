cd0948257cd4970ba6418fe9efab6749ef6f7590
Supraja Rachamalla <Supraja.Rachamalla@mu-sigma.com>
2015-09-06 01:08:57 +0530

    commited project

--- main.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('personCtrl', function($scope) {
-    $scope.message = "hello";
-    $scope.clicked = false;
-    $scope.save = function(){
-        $scope.clicked = true;
-    }
-});
